170efa2f95e5cc9f40fb02c6002644660db6fb8d
Natalia Zubareva <n.zubareva@inbox.ru>
2022-08-05 13:42:48 +0300

    security-methods is added

--- src/main/java/ru/netology/hibernate/config/SecurityConfiguration.java
+++ src/main/java/ru/netology/hibernate/config/SecurityConfiguration.java
@@ -2,45 +2,51 @@ package ru.netology.hibernate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Bean
     public UserDetailsService users() {
         UserDetails admin = User.builder()
                 .username("Natalia")
-                .password("{noop}pass")
-                .authorities("read","write")
+                .password(encoder().encode("pass"))
+                .roles("READ","WRITE", "DELETE")
                 .build();
-        UserDetails user = User.builder()
+        UserDetails user1 = User.builder()
                 .username("Ivan")
-                .password("{noop}1234")
-                .authorities("write")
+                .password(encoder().encode("1234"))
+                .roles("WRITE")
                 .build();
-        return new InMemoryUserDetailsManager(admin, user);
+        UserDetails user2 = User.builder()
+                .username("Vasilii")
+                .password(encoder().encode("5678"))
+                .roles("READ")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user1, user2);
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin()
                 .and()
-                .authorizeRequests().antMatchers("/persons/welcome").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-city").hasAuthority("read")
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-age").hasAuthority("write")
-                .and()
-                .authorizeRequests().antMatchers("/persons/name-and-surname").hasAuthority("write")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .csrf().disable();
         return http.build();
     }
 
--- src/main/java/ru/netology/hibernate/controller/DAOController.java
+++ src/main/java/ru/netology/hibernate/controller/DAOController.java
@@ -1,6 +1,9 @@
 package ru.netology.hibernate.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.netology.hibernate.entity.Person;
 import ru.netology.hibernate.service.DAOService;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,25 +22,32 @@ public class DAOController {
 
     private final DAOService service;
 
+    @Secured({"ROLE_READ"})
     @GetMapping("/by-city")
     public List<Person> getPersons(@RequestParam("city") String city) {
         return service.getPersonsByCity(city);
     }
 
+    @RolesAllowed({"ROLE_WRITE"})
     @GetMapping("/by-age")
     public List<Person> getAge(@RequestParam("age") int age) {
         return service.getAge(age);
     }
 
+
     @GetMapping("/name-and-surname")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public Optional<Person> getNameSurname(@RequestParam String name, @RequestParam String surname) {
         return service.getNameSurname(name, surname);
     }
 
     @GetMapping("/welcome")
-    public String getWelcome() {
-        return "Welcome";
+    @PostAuthorize("#username == authentication.principal.username")
+    public String getWelcome(String username) {
+        return "Welcome, " + username;
     }
+
+    @PreAuthorize("hasRole('ROLE_READ') or hasRole('ROLE_WRITE')")
     @GetMapping("/")
     public String get() {
         return "Hello from GET";
